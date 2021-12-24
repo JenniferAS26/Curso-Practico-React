a55331f74d2e3d7c3146e25d53fb5d41dcbfdf17
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2021-12-24 17:51:21 -0500

    useState to add to cart

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import bike from '@images/bike.jpg';
-import addCart from '@icons/bt_add_to_cart.svg';
+import addToCart from '@icons/bt_add_to_cart.svg';
 import '@styles/ProductItem.scss';
 
 const ProductItem = () => {
+	const [cart, setCart] = useState([]); // va a desencadenar el llamado al elemento
+	const handleClick = () => {
+		setCart([]);
+	}
     return (
         <div className="ProductItem">
 			<img src={bike}  alt="bike" />
@@ -12,8 +16,8 @@ const ProductItem = () => {
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure>
-					<img src={addCart} alt="add to cart" />
+				<figure onClick={handleClick} >
+					<img src={addToCart} alt="add to cart" />
 				</figure>
 			</div>
 		</div>
